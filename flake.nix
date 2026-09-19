1e7e64a311ca2274de03e3c6ccf0b4f695d624b8
shonenxnaifu <pawitrawarda@gmail.com>
2026-09-19 13:54:30 +0700

    add flake lock

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
   }:
   {
     # nixos-server adalah hostname
-    nixosConfiguration.nixos-server = nixpkgs.lib.nixosSystem {
+    nixosConfigurations.nixos-server = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
         ./configuration.nix
