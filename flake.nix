d5b2d7eaba5a960501198d57e284eeecb5e47435
shonenxnaifu <pawitrawarda@gmail.com>
2026-09-19 13:23:37 +0700

    add flake nix

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "NixOS Server Configuration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+  };
+
+  outputs = 
+  {
+    self,
+    nixpkgs,
+  }:
+  {
+    # nixos-server adalah hostname
+    nixosConfiguration.nixos-server = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+      ];
+    };
+  };
+}
