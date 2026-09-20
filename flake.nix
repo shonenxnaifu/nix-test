897773eb1c839fed41ef71f172919da0105c979f
shonenxnaifu <pawitrawarda@gmail.com>
2026-09-20 13:03:14 +0700

    add home manager

--- flake.nix
+++ flake.nix
@@ -3,12 +3,16 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+
+    home-manager.url = "github:nix-community/home-manager/release-26.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = 
   {
     self,
     nixpkgs,
+    home-manager,
   }:
   {
     # nixos-server adalah hostname
@@ -16,6 +20,13 @@
       system = "x86_64-linux";
       modules = [
         ./configuration.nix
+
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.vboxuser = import ./home.nix;
+        }
       ];
     };
   };
---
+++ home.nix
@@ -0,0 +1,28 @@
+{ config, pkgs, ...  }:
+
+{
+  home.username = "vboxuser";
+  home.homeDirectory = "/home/vboxuser/";
+
+  home.packages = with pkgs; [
+    ripgrep
+    fd
+    jq
+  ];
+
+  programs = {
+    bash = {
+      enable = true;
+      shellAliases = {
+        rebuild = "sudo nixos-rebuild switch --flake ~/nix#nixos-server";
+      };
+    };
+
+    lazygit = {
+      enable = true;
+      enableBashIntegration = true;
+    };
+  };
+
+  home.stateVersion = "26.05";
+}
