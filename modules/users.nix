755a23ab3e78f3debc42d6369bead4850773e81e
shonenxnaifu <pawitrawarda@gmail.com>
2026-09-21 16:00:22 +0700

    add ssh by key

--- modules/users.nix
+++ modules/users.nix
@@ -3,6 +3,9 @@
   users.users.vboxuser = {
     isNormalUser = true;
     extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-    initialPassword = "qwerty123";
+    # initialPassword = "qwerty123";
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDINTE5AAAAIJzmrK26arRwgJuzhB2QIZRlEyf05nBK3cKl2fuJo8p6 vboxuser"
+    ];
   };
 }
